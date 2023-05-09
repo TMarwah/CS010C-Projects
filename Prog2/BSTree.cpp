44b606de9444cb3f970d51182622686e3dc6d0d2
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-05-09 15:15:22 -0700

    prog2 comments begun

--- Prog2/BSTree.cpp
+++ Prog2/BSTree.cpp
@@ -8,12 +8,13 @@ BSTree::BSTree()
 : root(nullptr){}
 
 BSTree::~BSTree(){
-    //keep calling clear from root
+    //root clear, node destructor is seperate
     delete root;
     root = nullptr;
 
 }
 
+//insert value
 void BSTree::insert(const string& insertValue){
     if (root == nullptr){
         root = new Node(insertValue);
@@ -38,6 +39,7 @@ void BSTree::insert(const string& insertValue){
             curr = curr->left;
         }
         else{
+            //check case if node must be inserted as rightmost
             if (curr->right == nullptr){
                 curr->right = new Node(insertValue);
                 return;
@@ -47,21 +49,26 @@ void BSTree::insert(const string& insertValue){
     }
 }
 
+//search recursion
 bool BSTree::search(const string& searchVal) const{
     return searchHelp(root, searchVal);
 }
-
+//search recursion helper
 bool BSTree::searchHelp(Node* node, const string& searchVal) const{
     if (node == nullptr){
+        //node does not exist
         return false;
     }
     else if (searchVal == node->data){
+        //searchval has been found
         return true;
     }
     else if (searchVal < node->data){
+        //search left sub
         return searchHelp(node->left, searchVal);
     }
     else {
+        //search right sub
         return searchHelp(node->right, searchVal);
     }
 }
@@ -95,16 +102,19 @@ string BSTree::smallest() const{
     return curr->data;
 }
 
+//height recursion
 int BSTree::height(const string& heightVal, Node* curr) const {
+    //not found
     if (curr == nullptr){
         return -1;
     }
-
+    //determine left and right subtree distance, then determine the larger one (most hopes required)
     int lsbtHeight = height(heightVal, curr->left);
     int rsbtHeight = height(heightVal,curr->right);
     return 1 + max(lsbtHeight,rsbtHeight);
 }
 
+//height helper
 int BSTree::height(const string& key) const{
     //if key does not exist, return -1
     if (!search(key)){
@@ -129,7 +139,7 @@ int BSTree::height(const string& key) const{
         return height(key,curr);
 
 }
-
+//remove helper
 void BSTree::remove(Node *&curr, const string &key, bool cont)
 {
     if (!curr)
@@ -139,7 +149,7 @@ void BSTree::remove(Node *&curr, const string &key, bool cont)
         remove(curr->left, key, cont);
     }
     else if (key > curr->data)
-    { // Keep on searching for node to remove (to right     )
+    { // Keep on searching for node to remove (to right)
         remove(curr->right, key, cont);
     }
     else
@@ -178,6 +188,7 @@ void BSTree::remove(Node *&curr, const string &key, bool cont)
         }
     }
 }
+//remove recursion
 void BSTree::remove(const string &key)
 {
     remove(root, key, false);
@@ -200,7 +211,6 @@ void BSTree::postOrder() const {
 
 void BSTree::preOrder(Node* node) const {
     if (node != nullptr) {
-        //start root
         //print helper
         cout << node->data << "(" << node->count << "), ";
         //left subtree
@@ -213,8 +223,11 @@ void BSTree::preOrder(Node* node) const {
 
 void BSTree::postOrder(Node* node) const {
     if (node != nullptr) {
+        //traverse left sub
         postOrder(node->left);
+        //traverse right sub
         postOrder(node->right);
+        //print stack
         cout << node->data << "(" << node->count << "), ";
     }
 }
