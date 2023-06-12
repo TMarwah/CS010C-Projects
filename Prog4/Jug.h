e07f5639b6c97d0475606c00ad6d59efcfba8c81
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-06-11 18:00:24 -0700

    prog4 complete

--- Prog4/Jug.h
+++ Prog4/Jug.h
@@ -1,7 +1,26 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <list>
 #include <string>
 
 using namespace std;
+
+struct Vertex{
+    //list of int pairs to denote neighbors
+    list<pair<int,int>> neighbors;
+
+    int jugA;
+    int jugB;
+    int distance;
+    string decision;
+    Vertex* prev;
+
+    //vertex constructor
+    Vertex();
+    //vertex destructor
+    ~Vertex();
+};
 class Jug {
     public:
         Jug(int,int,int,int,int,int,int,int,int);
@@ -13,5 +32,19 @@ class Jug {
         //returns 1 if solution is found and stores solution steps in solution string.
         int solve(string &solution);
     private:
-        //anything else you need
+    
+        int Ca; //a cap
+        int Cb; //b cap
+        int N; //goal
+        int cfA; //fill cost a
+        int cfB; //fill cost b
+        int ceA; //empty cost a
+        int ceB; //empty cost b
+        int cpAB; //pour cost a -> b
+        int cpBA; //pour cost b -> a
+        void makeGraph(Vertex *);
+        bool valid(); //check op cost for validity
+        void reset(vector<Vertex*> vertices);
+        vector<Vertex*> vertices; //determine op possitibilites
+        vector<string> paths; //determined op path
 };
--- Prog4/main.cpp
+++ Prog4/main.cpp
@@ -1,19 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<utility>
 #include "Jug.h"
-#include <iostream>
 using namespace std;
 
 int main(){
-    string solution1;
-    Jug head(3, 5, 4, 1, 2, 3, 4, 5, 6);
-    if (head.solve(solution1) != 1){
-        cout << "Error 3" << endl;
-    }
-    cout << solution1 << endl << endl;
-
-    string solution2;
-    Jug head(3, 5, 4, 1, 1, 1, 1, 1, 2);
-    if (head.solve(solution2) != 1){
-        cout << "Error 3" << endl;
-    }
-    cout << solution2 << endl;
+  {
+   string solution;
+   Jug head(3, 5, 4, 1, 2, 3, 4, 5, 6);
+   if (head.solve(solution) != 1) {
+      cout << "Error 3" << endl;
+   }
+   cout << solution << endl << endl;
+}
+{
+   string solution;
+   Jug head( 3, 5, 4, 1, 1, 1, 1, 1, 2);         
+   if(head.solve(solution) != 1) {
+      cout << "Error 3" << endl;
+   }
+   cout << solution << endl;
+}
 }
